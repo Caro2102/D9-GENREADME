c637697e81eee80ff17367df4848e24235e542fb
Carolina Cruz <caro21_cruz@hotmail.com>
2022-11-14 02:11:28 -0600

    Se corrigio error en licencias

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,9 +4,9 @@ function renderLicenseBadge(license) {
   if (license==="Apache"){
     return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
   }else if (license==="Boost"){
-    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt";
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
   }else if (license==="Mozilla"){
-    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0";
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
   }else if (license==="Eclipse"){
     return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
   }else if (license==="MIT"){
@@ -20,9 +20,9 @@ function renderLicenseLink(license) {
   if (license==="Apache"){
     return "[link](https://opensource.org/licenses/Apache-2.0)";
   }else if (license==="Boost"){
-    return "[link](https://www.boost.org/LICENSE_1_0.txt";
+    return "[link](https://www.boost.org/LICENSE_1_0.txt)";
   }else if (license==="Mozilla"){
-    return "[link](https://opensource.org/licenses/MPL-2.0";
+    return "[link](https://opensource.org/licenses/MPL-2.0)";
   }else if (license==="Eclipse"){
     return "[link](https://opensource.org/licenses/EPL-1.0)";
   }else if (license==="MIT"){
